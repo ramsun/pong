39173ba76da41dd3af6f7095ac45c880d159ddd3
ramsun <rsramamurthysundar@gmail.com>
2014-12-30 02:51:11 -0500

    Final

--- Game.py
+++ Game.py
@@ -1,20 +1,25 @@
-import sys, pygame, time
-pygame.init()
-myFont = pygame.font.SysFont("wingdings2", 150)
+""" Game.py is a pong game that is fully customizable.  The global constants are easily accessable and can be fine tuned to fit one's needs. 
 
-size = width, height = 800, 600
-speed = [2, 2]
-black = 0, 0, 0
-white = 255, 255, 255
+Created by Ramamurthy Sundar on December 30, 2014.
+"""
+
+import sys, pygame, time
+
+pygame.init()
+myFont = pygame.font.SysFont("impact", 150)
+
+# ARGUMENTS
+BALL_SPEED = 5
+screen_size = width, height = 1100, 600
 PADDLE_WIDTH = 8
 PADDLE_HEIGHT = 100
 DISTANCE_SCREEN = 8
 
-
-screen = pygame.display.set_mode(size)
-
-# def drawSolidLine(screen, width, height):
-	# pygame.draw.lines(screen, white, False, [(width/2, 0), (width/2, height)], 3)
+NUMBER_OF_SEGMENTS = 40
+black = 0, 0, 0
+white = 255, 255, 255
+speed = [BALL_SPEED, BALL_SPEED]
+screen = pygame.display.set_mode(screen_size)
 
 def equalSpaceDashGen(screen, numSegments, startPoint, endPoint):
 	"""Creates a dashed line that is meant to represent the net for the tennis match"""
@@ -42,13 +47,14 @@ def playerScores(leftScore, rightScore):
 	rightLabel = myFont.render(str(rightScore), 1, white)
 
 	screen.blit(leftLabel, (width/2-150, 8))
-	screen.blit(leftLabel, (width/2+77, 8))
+	screen.blit(rightLabel, (width/2+77, 8))
 
 def main():
 
-	print pygame.font.get_fonts()
+	leftScore = 0
+	rightScore = 0
 
-	ball = pygame.image.load("ball.gif")
+	ball = pygame.image.load("ball.png")
 	ballrect = ball.get_rect()
 	INITIAL_POSITION = height/2
 	heightLeft = INITIAL_POSITION
@@ -56,6 +62,8 @@ def main():
 	SPEED = 3
 
 	while 1:
+
+		# moves the paddles with key controls
 		key = pygame.key.get_pressed()
 		if key[pygame.K_UP] and heightRight>0:
 			heightRight -=  SPEED
@@ -66,25 +74,41 @@ def main():
 		if key[pygame.K_s] and heightLeft < (height-PADDLE_HEIGHT):
 			heightLeft += SPEED
 
+		# reflects the ball off of the left and right paddles 
+		if (0 < ballrect.right - (width - (PADDLE_WIDTH + DISTANCE_SCREEN)) < BALL_SPEED) \
+			and (ballrect.bottom > heightRight) and (ballrect.top < heightRight+PADDLE_HEIGHT):
+			speed[0] = -speed[0]
 
+		if (-BALL_SPEED < ballrect.left - (PADDLE_WIDTH + DISTANCE_SCREEN) < 0) \
+			and (ballrect.bottom > heightLeft) and (ballrect.top < heightLeft+PADDLE_HEIGHT):
+			speed[0] = -speed[0]
 
+		#quits if you want to quit
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT: sys.exit()
 
 		ballrect = ballrect.move(speed)
-		if ballrect.left < 0 or ballrect.right > width:
-			speed[0] = -speed[0]
+
+		# increases the persons score who wins by 1
+		if ballrect.right < 0:
+			rightScore += 1
+		elif ballrect.left > width:
+			leftScore += 1
+
+		#resets the ball to the center if it goes off screen
+		if ballrect.right < 0 or ballrect.left > width:
+			ballrect.centerx = width/2
+			ballrect.centery = height/2
+
 		if ballrect.top < 0 or ballrect.bottom > height:
 			speed[1] = -speed[1]
 
-
-	
 		screen.fill(black)
-		#drawSolidLine(screen, width, height)
-		equalSpaceDashGen(screen, 40, (width/2, 0), (width/2, height))
+		equalSpaceDashGen(screen, NUMBER_OF_SEGMENTS, (width/2, 0), (width/2, height))
 		paddleMovement(screen, heightLeft, heightRight)
-		playerScores(0, 0)
+		playerScores(leftScore, rightScore)
 		screen.blit(ball, ballrect)
 		pygame.display.flip()
 
-main()
+if __name__ == "__main__":
+	main()
---
+++ README.md
@@ -0,0 +1,8 @@
+Pong
+====
+
+![Pong](Screenshot.jpg)
+
+This is my first python project, so I thought I would make a pong game. Feel free to use the code however you want.  
+
+It's fully customizable and you can change almost anything that is possible to change in a pong game.
