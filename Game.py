417756a780a3de045846ee9b37ec295af97f4d41
ramsun <rsramamurthysundar@gmail.com>
2014-12-29 22:36:25 -0500

    Addition of Speed From Website
    Speed of ball and background fill

--- Game.py
+++ Game.py
@@ -1,7 +1,26 @@
-print "Hello World"
+import sys, pygame, time
+pygame.init()
 
-x = 27
+size = width, height = 1080, 900
+speed = [2, 2]
+black = 0, 0, 0
+white = 255, 255, 255
 
-y = x
+screen = pygame.display.set_mode(size)
 
-print x**y
+ball = pygame.image.load("ball.gif")
+ballrect = ball.get_rect()
+
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+
+    ballrect = ballrect.move(speed)
+    if ballrect.left < 0 or ballrect.right > width:
+        speed[0] = -speed[0]
+    if ballrect.top < 0 or ballrect.bottom > height:
+        speed[1] = -speed[1]
+
+    screen.fill(white)
+    screen.blit(ball, ballrect)
+    pygame.display.flip()
