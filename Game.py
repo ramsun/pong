87e7cf466464d37c271f945c5eb7426f6b90c72b
ramsun <rsramamurthysundar@gmail.com>
2014-12-30 01:34:46 -0500

    Addition of movement of paddles and score counter
    Players can now move the paddles and scores are posted on each half of
    the court.

--- Game.py
+++ Game.py
@@ -1,15 +1,17 @@
 import sys, pygame, time
 pygame.init()
+myFont = pygame.font.SysFont("wingdings2", 150)
 
 size = width, height = 800, 600
 speed = [2, 2]
 black = 0, 0, 0
 white = 255, 255, 255
+PADDLE_WIDTH = 8
+PADDLE_HEIGHT = 100
+DISTANCE_SCREEN = 8
+
 
 screen = pygame.display.set_mode(size)
-
-ball = pygame.image.load("ball.gif")
-ballrect = ball.get_rect()
 
 # def drawSolidLine(screen, width, height):
 	# pygame.draw.lines(screen, white, False, [(width/2, 0), (width/2, height)], 3)
@@ -27,19 +29,62 @@ def equalSpaceDashGen(screen, numSegments, startPoint, endPoint):
 		pygame.draw.lines(screen, white, False, [(width/2, segmentStart), (width/2, segmentStart + whiteSegment )], 3)
 		segmentStart += black_white_seg
 
-while 1:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+def paddleMovement(screen, heightLeft, heightRight):
+	"""Creates the initial positions of the two rectangles """
 
-    ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = -speed[1]
+	pygame.draw.rect(screen, white, (DISTANCE_SCREEN,heightLeft,PADDLE_WIDTH, PADDLE_HEIGHT), 0)
+	pygame.draw.rect(screen, white, ((width-(DISTANCE_SCREEN+PADDLE_WIDTH)),heightRight,PADDLE_WIDTH,PADDLE_HEIGHT), 0)
 
-    
-    screen.fill(black)
-    #drawSolidLine(screen, width, height)
-    equalSpaceDashGen(screen, 40, (width/2, 0), (width/2, height))
-    screen.blit(ball, ballrect)
-    pygame.display.flip()
+
+def playerScores(leftScore, rightScore):
+	""" Prints the scores of each player onto the game """
+	leftLabel = myFont.render(str(leftScore), 1, white)
+	rightLabel = myFont.render(str(rightScore), 1, white)
+
+	screen.blit(leftLabel, (width/2-150, 8))
+	screen.blit(leftLabel, (width/2+77, 8))
+
+def main():
+
+	print pygame.font.get_fonts()
+
+	ball = pygame.image.load("ball.gif")
+	ballrect = ball.get_rect()
+	INITIAL_POSITION = height/2
+	heightLeft = INITIAL_POSITION
+	heightRight = INITIAL_POSITION
+	SPEED = 3
+
+	while 1:
+		key = pygame.key.get_pressed()
+		if key[pygame.K_UP] and heightRight>0:
+			heightRight -=  SPEED
+		if key[pygame.K_DOWN] and heightRight < (height-PADDLE_HEIGHT):
+			heightRight += SPEED
+		if key[pygame.K_w] and heightLeft>0:
+			heightLeft -=  SPEED
+		if key[pygame.K_s] and heightLeft < (height-PADDLE_HEIGHT):
+			heightLeft += SPEED
+
+
+
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT: sys.exit()
+
+		ballrect = ballrect.move(speed)
+		if ballrect.left < 0 or ballrect.right > width:
+			speed[0] = -speed[0]
+		if ballrect.top < 0 or ballrect.bottom > height:
+			speed[1] = -speed[1]
+
+
+	
+		screen.fill(black)
+		#drawSolidLine(screen, width, height)
+		equalSpaceDashGen(screen, 40, (width/2, 0), (width/2, height))
+		paddleMovement(screen, heightLeft, heightRight)
+		playerScores(0, 0)
+		screen.blit(ball, ballrect)
+		pygame.display.flip()
+
+main()
