2a68b1bfe3725cae0453af839f9ae82952b3f535
ramsun <rsramamurthysundar@gmail.com>
2014-12-30 00:13:43 -0500

    Addition of Dash Line Function
    Function added that creates a tennis net for the pong game.  Also has
    the ability to change the ratio of black space for each segment.

--- Game.py
+++ Game.py
@@ -1,7 +1,7 @@
 import sys, pygame, time
 pygame.init()
 
-size = width, height = 1080, 900
+size = width, height = 800, 600
 speed = [2, 2]
 black = 0, 0, 0
 white = 255, 255, 255
@@ -10,6 +10,22 @@ screen = pygame.display.set_mode(size)
 
 ball = pygame.image.load("ball.gif")
 ballrect = ball.get_rect()
+
+# def drawSolidLine(screen, width, height):
+	# pygame.draw.lines(screen, white, False, [(width/2, 0), (width/2, height)], 3)
+
+def equalSpaceDashGen(screen, numSegments, startPoint, endPoint):
+	"""Creates a dashed line that is meant to represent the net for the tennis match"""
+	RATIO = 0.66
+	fullSize = endPoint[1] - startPoint[1]
+	black_white_seg = fullSize/numSegments
+
+	whiteSegment = RATIO * black_white_seg
+	
+	segmentStart = startPoint[1];
+	while  segmentStart < endPoint[1]:
+		pygame.draw.lines(screen, white, False, [(width/2, segmentStart), (width/2, segmentStart + whiteSegment )], 3)
+		segmentStart += black_white_seg
 
 while 1:
     for event in pygame.event.get():
@@ -21,6 +37,9 @@ while 1:
     if ballrect.top < 0 or ballrect.bottom > height:
         speed[1] = -speed[1]
 
-    screen.fill(white)
+    
+    screen.fill(black)
+    #drawSolidLine(screen, width, height)
+    equalSpaceDashGen(screen, 40, (width/2, 0), (width/2, height))
     screen.blit(ball, ballrect)
     pygame.display.flip()
